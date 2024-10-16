88f2d493bc5c329da07fbe66533a19498047cb98
Dylan <dotfords@gmail.com>
2024-10-16 19:11:42 +0200

    commit function creation.

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,19 +1,93 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
+let presupuesto = 0;
+let gastos = new Array();
+let idGasto = 0;
 
 // TODO: Variable global
 
+// FUNCIONES GLOBALES
+function actualizarPresupuesto(nuevoPresupuesto) {
+    
+    if (typeof nuevoPresupuesto == 'number' && nuevoPresupuesto >= 0 ) {
+        presupuesto = nuevoPresupuesto;
+        return presupuesto;
 
-function actualizarPresupuesto() {
-    // TODO
+    }
+    else{
+        console.error("El valor introducido no es valido");
+        return -1;
+    }
 }
 
 function mostrarPresupuesto() {
-    // TODO
+    return "Tu presupuesto actual es de " + presupuesto + " €";
+    
 }
 
-function CrearGasto() {
-    // TODO
+function CrearGasto(descripcion, valor, fecha, etiquetas = []) {
+    if (valor <= 0){
+        valor = 0
+    }
+
+    if (Date.parse(fecha)){
+        fecha = Date.parse(fecha);
+    }
+    else{
+        fecha = Date.now();
+    }
+    let gasto = {
+        descripcion: descripcion,
+        valor: valor,
+        fecha: fecha,
+        etiquetas: etiquetas,
+
+        mostrarGasto: function() {
+            return "Gasto correspondiente a " + this.descripcion + " con valor " + this.valor + " €";
+            
+        },
+        actualizarDescripcion: function(nuevaDescripcion){
+            this.descripcion = nuevaDescripcion;
+        
+        },
+        actualizarValor: function(nuevoValor){
+            if (nuevoValor >= 0){
+                this.valor = nuevoValor;
+            }
+            else{
+                console.error("El valor introducido no es valido.");
+                return -1;
+            }
+        },
+        anyadirEtiquetas: function() {
+
+        }
+    }
+
 }
+
+function anyadirGasto(gasto){
+    gastos.id = idGasto;
+
+    idGasto++;
+
+    gastos.push(gasto);
+}
+function listarGastos(){
+    return gastos;
+}
+
+function borrarGasto(gasto){
+    gastos.pop(gasto.id)
+}
+function calcularBalance(){
+
+}
+function calcularTotalGastos(){
+
+}
+
+
+
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
@@ -21,5 +95,10 @@ function CrearGasto() {
 export   {
     mostrarPresupuesto,
     actualizarPresupuesto,
-    CrearGasto
+    CrearGasto,
+    listarGastos,
+    anyadirGasto,
+    borrarGasto,
+    calcularTotalGastos,
+    calcularBalance
 }
